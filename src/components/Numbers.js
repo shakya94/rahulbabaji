0994d491f43155035915163190d0aa6c19318d75
shakya94 <57297686+shakya94@users.noreply.github.com>
2024-03-27 21:39:48 +0530

    tooltip

--- src/components/Numbers.js
+++ src/components/Numbers.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Tooltip } from 'react-tooltip';
 
 function Numbers() {
   const total = 43567;
@@ -10,15 +11,29 @@ function Numbers() {
     { name: 'Digestive Issues', no: '857' },
     { name: 'Migraine', no: '574' },
   ];
-
+  const tool = [
+    { Cancer: 'Skin Cancer', Hairfall: '456' },
+    { Cancer: 'Blood Cancer', Hairfall: '456' },
+    { Cancer: 'Lung Cancer', Hairfall: '456' },
+    { Cancer: 'Mouth Cancer', Hairfall: '456' },
+    { Cancer: 'Breast Cancer', Hairfall: '456' },
+    { Cancer: 'All Cancer', Hairfall: '456' },
+  ];
   return (
     <div className='py-20'>
       <div className="headers mt-4">Total Patients Cured<br/>{total}</div>
       <div className='row m-4' style={{ justifyContent: 'space-around' }}>
         {urlList.map((tempNo, index) => (
-          <div key={index} className="no col-12 col-xl-4 mb-4 p-4 d-flex justify-content-center align-items-center">
+          <div key={index} className="my-anchor-element no col-12 col-xl-4 mb-4 p-4 d-flex justify-content-center align-items-center">
             <h4>{tempNo.name}:<br />
-            <div className='d-flex justify-content-center align-items-center'>{tempNo.no}</div></h4>
+              <div className='d-flex justify-content-center align-items-center'>{tempNo.no}</div>
+            </h4>
+            <Tooltip anchorSelect=".my-anchor-element" place="right">
+            {tool.map((con, index) => (
+  con[tempNo.name] === tempNo.name ? <div key={index}>{con[tempNo.name]}</div> : null
+))}
+
+            </Tooltip>
           </div>
         ))}
       </div>
